ab1add17fb297997476c2215f6c2abd04953e21b
Juan Camilo Calero Espinosa <jccaleroe@unal.edu.co>
2019-12-12 11:16:32 -0500

    not sarcastic

--- albert/remove_same_headlines.py
+++ albert/remove_same_headlines.py
@@ -1,12 +1,12 @@
 d = {}
-with open('sarcasm_headlines/all.tsv') as f:
+with open('sarcasm_headlines/test_spanish.tsv') as f:
     f.readline()
     for i in f:
         tmp = i.split('\t')
         d[tmp[1][:-1]] = tmp[0]
 
 print(len(d))
-print(d['internet charmed by viral photo of teen working to pay for first real date'])
+print(d['Es mejor que digan cosas malas sobre ti, a que no digan nada'])
 with open('sarcasm_headlines/all2.tsv', 'w') as f:
     for i in d:
         f.write(d[i])
