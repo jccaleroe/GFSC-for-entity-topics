471135e1a368f0685c429445103d0ed09dba3ca1
Juan Camilo Calero Espinosa <jccaleroe@unal.edu.co>
2019-12-12 23:51:37 -0500

    cleansing

--- albert/remove_same_headlines.py
+++ albert/remove_same_headlines.py
@@ -1,13 +1,11 @@
 d = {}
-with open('sarcasm_headlines/test_spanish.tsv') as f:
-    f.readline()
+with open('sarcasm_headlines/tmp') as f:
     for i in f:
         tmp = i.split('\t')
-        d[tmp[1][:-1]] = tmp[0]
+        d[tmp[1][:-1].lower()] = tmp[0]
 
 print(len(d))
-print(d['Es mejor que digan cosas malas sobre ti, a que no digan nada'])
-with open('sarcasm_headlines/all2.tsv', 'w') as f:
+with open('sarcasm_headlines/tmp2.tsv', 'w') as f:
     for i in d:
         f.write(d[i])
         f.write('\t')
