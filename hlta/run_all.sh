cb0805363cc1eba4339616f2d7340509cea6b2a7
Juan Camilo Calero Espinosa <jccaleroe@unal.edu.co>
2020-08-15 19:07:11 -0500

    views by level

--- hlta/run_all.sh
+++ hlta/run_all.sh
@@ -13,38 +13,36 @@ fi
 
 #Modify according to your dataset
 prefixes=("verb" "org" "loc" "per" "adj")
-#prefixes=("verb")
 
-#Modify according how important is each hierarchy for you
+#Modify according on how many words to use is each hierarchy
 
-#for the silla vacia
+#for the profiles
 words_per_type=(1000 800 800 800 400)
-#words_per_type=(1000)
 
 #for the abstracts
 #words_per_type=(600 500 200 200 200 300 200)
 
-for i in ${prefixes[@]}; do
-  mkdir -p topic_models/"$2"/$i
-  rm -rf topic_models/"$2"/$i/*
+for i in "${prefixes[@]}"; do
+  mkdir -p topic_models/"$2"/"$i"
+  rm -rf topic_models/"$2"/"$i"/*
 done
 
-for i in ${!prefixes[@]}; do
-  sh hlta-es.sh "$1"/${prefixes[$i]} topic_models/"$2"/${prefixes[$i]} ${words_per_type[$i]} 0 4 4
+for i in "${!prefixes[@]}"; do
+  sh hlta-es.sh "$1"/"${prefixes[$i]}" topic_models/"$2"/"${prefixes[$i]}" "${words_per_type[$i]}" 0 4 4
 done
 
 #mkdir -p ../fusion/"$2"/evaluation
 rm -rf ../fusion/"$2"/*
 touch ../fusion/"$2"/myData.sparse.txt
 
-for i in ${prefixes[@]}; do
-  mkdir -p ../fusion/"$2"/$i
-  cp topic_models/"$2"/$i/myAssignment.topics.json ../fusion/"$2"/$i/
-  cp topic_models/"$2"/$i/myData.dict.csv ../fusion/"$2"/$i/
-  cp topic_models/"$2"/$i/myData.files.txt ../fusion/"$2"/$i/
-  cp topic_models/"$2"/$i/topicTree.nodes.json ../fusion/"$2"/$i/
-  cp topic_models/"$2"/$i/myModel.bif ../fusion/"$2"/$i/
-  cat topic_models/"$2"/$i/myData.sparse.txt >> ../fusion/"$2"/myData.sparse.txt
+for i in "${prefixes[@]}"; do
+  mkdir -p ../fusion/"$2"/"$i"
+  cp topic_models/"$2"/"$i"/myAssignment.topics.json ../fusion/"$2"/"$i"/
+  cp topic_models/"$2"/"$i"/myData.dict.csv ../fusion/"$2"/"$i"/
+  cp topic_models/"$2"/"$i"/myData.files.txt ../fusion/"$2"/"$i"/
+  cp topic_models/"$2"/"$i"/topicTree.nodes.json ../fusion/"$2"/"$i"/
+  cp topic_models/"$2"/"$i"/myModel.bif ../fusion/"$2"/"$i"/
+  cat topic_models/"$2"/"$i"/myData.sparse.txt >> ../fusion/"$2"/myData.sparse.txt
 done
 
 sort -u ../fusion/"$2"/myData.sparse.txt | sort -g -o ../fusion/"$2"/myData.sparse.txt
