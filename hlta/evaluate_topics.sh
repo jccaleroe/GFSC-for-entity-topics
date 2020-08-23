13c045836a294cf1fe165c544076467be651d391
Juan Camilo Calero Espinosa <jccaleroe@unal.edu.co>
2020-08-23 18:32:34 -0500

    level 1 nodes

--- hlta/evaluate_topics.sh
+++ hlta/evaluate_topics.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#usage: topics, sparse, target_file
+
 echo "Topic coherence:" >> "$3"
-java -Xms2G -Xmx8G -cp HLTA.jar:HLTA-deps.jar tm.hlta.TopicCoherence "$1" "$2" >> "$3"
+java -Xms2G -Xmx8G -cp HLTA-es.jar:HLTA-deps.jar tm.hlta.TopicCoherence "$1" "$2" >> "$3"
 echo "Topic compactness:" >> "$3"
-java -Xmx8G -cp HLTA.jar:HLTA-deps.jar tm.hlta.TopicCompactness "$1" GoogleNews-vectors-negative300.bin >> "$3"
+java -Xmx8G -cp HLTA-es.jar:HLTA-deps.jar tm.hlta.TopicCompactness "$1" GoogleNews-vectors-negative300.bin >> "$3"
 
